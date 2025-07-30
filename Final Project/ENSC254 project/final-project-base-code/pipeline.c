7b3670e27a33ef9c664fe2851490ab6c5bbf49ec
RMJun5 <richard.mk.cwc@gmail.com>
2025-07-30 13:21:23 -0700

    Updated with past lab files

--- Final Project/ENSC254 project/final-project-base-code/pipeline.c
+++ Final Project/ENSC254 project/final-project-base-code/pipeline.c
@@ -38,6 +38,7 @@ ifid_reg_t stage_fetch(pipeline_wires_t* pwires_p, regfile_t* regfile_p, Byte* m
   /**
    * YOUR CODE HERE
    */
+  
 
   #ifdef DEBUG_CYCLE
   printf("[IF ]: Instruction [%08x]@[%08x]: ", instruction_bits, regfile_p->PC);
